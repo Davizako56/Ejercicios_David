84ebca89e006c152cf5235b1dd9b4031d596a523
A717\1dam <davidginer.alicante@gmail.com>
2024-10-15 18:50:05 +0200

    Altura ejercicio

--- Eejercicio/src/main/java/org/example/Main.java
+++ Eejercicio/src/main/java/org/example/Main.java
@@ -1,14 +1,14 @@
 package org.example;
 
-import org.example.Unidad2.Excepciones;
-import org.example.Unidad2.Und2Practica1;
+import org.example.Unidad2.Altura;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
 
-        Und2Practica1 und2practica1 = new Und2Practica1();
-        Und2Practica1.ejecuter();
+        Altura altura = new Altura();
+        Altura.ejecuter();
     }
 }
---
+++ Eejercicio/src/main/java/org/example/Unidad2/Altura.java
@@ -0,0 +1,32 @@
+package org.example.Unidad2;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Altura {
+    public static void main(String[] args) {
+    }
+    public static void ejecuter() {
+        Scanner entrada = new Scanner(System.in);
+        int a = 0;
+        boolean err = true;
+
+        while (err == true) {
+            try {
+                System.out.println("Introduzca su altura...");
+                a = entrada.nextInt();
+                err = false;
+            }catch (InputMismatchException er) {
+                System.out.println("ERROR: El programa no admite texto.");
+                entrada.nextLine();
+            }
+        }
+
+        if(a <= 150) {
+            System.out.println("Persona de altura baja.");
+        }else if(a >= 151 && a <= 175) {
+            System.out.println("Persona de altura media");
+        }else if(a > 175) {
+            System.out.println("Persona alta");
+        }
+    }
+}
