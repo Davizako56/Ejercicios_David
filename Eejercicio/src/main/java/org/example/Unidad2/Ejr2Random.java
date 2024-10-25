3848867029a8eeaebec472064e9c003445f90b04
A717\1dam <davidginer.alicante@gmail.com>
2024-10-25 15:52:31 +0200

    Random ejr 4

--- Eejercicio/src/main/java/org/example/Unidad2/Ejr2Random.java
+++ Eejercicio/src/main/java/org/example/Unidad2/Ejr2Random.java
@@ -6,11 +6,12 @@ public class Ejr2Random {
     public static void ejecuter() {
         Scanner entrada = new Scanner(System.in);
         Random random = new Random();
+        int rang = random.nextInt(6,9);
 
         int i;
         String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-        for (i = 0; i < 10; i++) {
+        for (i = 0; i < rang; i++) {
 
             int num = random.nextInt(0,61);
             String code = caracteres.substring(num,num + 1);
