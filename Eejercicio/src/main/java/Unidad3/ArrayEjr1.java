5eeace6328f16b89d0d1f828ddd8373b29a84a27
A717\1dam <davidginer.alicante@gmail.com>
2024-11-05 17:44:41 +0100

    Array Ejr 1

---
+++ Eejercicio/src/main/java/Unidad3/ArrayEjr1.java
@@ -0,0 +1,20 @@
+package Unidad3;
+import java.util.Random;
+
+public class ArrayEjr1 {
+    public static void ejecuter() {
+        Random random = new Random();
+
+        int vector[] = new int[8];
+        int i = 0;
+        int res = 0;
+
+        while(i < 8) {
+            int num = random.nextInt(0,501);
+            vector[i] = num;
+            res += vector[i];
+            i++;
+        }
+        System.out.println("La suma de los elementos es: " + res);
+    }
+}
---
+++ Eejercicio/src/main/java/Unidad3/Main.java
@@ -0,0 +1,25 @@
+package Unidad3;
+
+import org.example.Unidad2.Bruh;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayEjr1 arrayejr1 = new ArrayEjr1();
+        ArrayEjr1.ejecuter();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
