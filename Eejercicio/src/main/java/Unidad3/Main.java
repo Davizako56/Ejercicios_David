2223d24742dadc419d91a1cb35ad4dda0dade12f
A717\1dam <davidginer.alicante@gmail.com>
2024-11-07 17:20:41 +0100

    Array Ejr 5

--- Eejercicio/src/main/java/Unidad3/Main.java
+++ Eejercicio/src/main/java/Unidad3/Main.java
@@ -8,8 +8,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayEjr4 arrayejr4 = new ArrayEjr4();
-        ArrayEjr4.ejecuter();
+        ArrayEjr5 arrayejr5 = new ArrayEjr5();
+        ArrayEjr5.ejecuter();
     }
 }
 
